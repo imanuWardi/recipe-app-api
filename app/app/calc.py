45460ee1e1854b5b77acbe9f38f2ada1e4171b00
Imanu Wardi <149247547+imanuWardi@users.noreply.github.com>
2025-12-19 00:19:10 +0700

    write a tests using TDD

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+'''
+Calculator functions
+'''
+
+def add(x, y):
+    '''add x and y and return the result.'''
+    return x + y
+
+def subtract(x, y):
+    '''subtract x and y and return the result.'''
+    return x - y
